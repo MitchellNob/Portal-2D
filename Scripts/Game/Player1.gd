3718809d40a57889e3d8f81454280abb39a13101
MitchellNob <moustache.mitchell@gmail.com>
2022-10-20 12:59:48 +1100

    Added comments
    I am adding comments to the code, Haven't finished I also deleted the _Phyics process from the script

--- Scripts/Game/Player1.gd
+++ Scripts/Game/Player1.gd
@@ -1,22 +1,33 @@
 extends KinematicBody2D
 
+#This is the Max walk speed that our character can travel
 var Max_Walkspeed = 200
+#This is a speed that will change as the character accels towards the Max_Walkspeed
 var speed = 0
+#This is another variable that will increase the characters max speed if they press shift
 var Max_Sprintspeed = 300
+#This is the acceleration at which our character will begin to speed up to
 var acceleration = 1
+#This is the direction that we will be moving in it is (0,0) or .ZERO
 var move_direction = Vector2(0,0)
-var NoShoot = false
 
+#Here I call upon different Nodes within my scene so that and turn the into a variable
+#so I can use them later on
 onready var anim_sprite = $AnimationPlayer
 onready var EndGun = $Head/Gun/EndGun
 
+#A packed scene is a simplified way of calling for a scene I can go into the inspector
+#and assign the scene to the variable
 export (PackedScene) var BlueBullet
 
+#The start function can only run as the script is called, the _phyiscs_process function
+#is by default run 60 times every second, this means that physics mechanics, such as 
+#movement is much smoother
 func _physics_process(delta):
+#Here we call the movement, animation and aim function as they all need to be 
+#processed 
 	Movement(delta)
-#	Animation(delta) 
-
-func _process(delta):
+#Animation(delta) 
 	Aim(delta)
 
 func Movement(delta):
--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,5 +1,7 @@
 extends Node
 
+#this is the boolean variable that we will call within different scripts to make sure 
+#that the Bullet can actually be fired
 var BluePortal = true
 
 func _ready():
