2d9766165ce33f5b714855c9d20766418059474a
MitchellNob <moustache.mitchell@gmail.com>
2022-10-20 23:57:29 +1100

    added some more comments

--- Scripts/Game/Player1.gd
+++ Scripts/Game/Player1.gd
@@ -31,34 +31,69 @@ func _physics_process(delta):
 	Aim(delta)
 
 func Movement(delta):
+#move_direction is the vector we set before, here we are assigning the x axis of the vector by 
+#getting the number or integer input depending on the movement button we press
 	move_direction.x = int(Input.is_action_pressed("Right")) - int(Input.is_action_pressed("Left"))
+#this is just doing the same thing however for the y axis
 	move_direction.y = int(Input.is_action_pressed("Down")) - int(Input.is_action_pressed("Up"))
+#here we are using an if statement that says if the move_direction = .ZERO or the character is 
+#no movement keys being inputed than we set the speed to 0 so that the character doesn't continue to move
 	if move_direction == Vector2(0,0):
 		speed = 0
+#We than use an else statement for anytime that the above if statement is false, here if the
+#vector2 > 0 than we are timesing the speed by the acceleration * delta, the delta is part
+#of the _physics_process, this allows the movement to not depend on frame rate and instead 
+#will update 60 times a second, with varying frame rates something that relies on frames
+#can fluctuate so it is better to use delta
 	else:
 		speed += acceleration * delta
+#here we have another if statement that states that if the shift key is press than the speed is
+#equal to the variable Max_sprintspeed instead of the speed
 		if  int(Input.is_action_pressed("Sprint")):
 			speed = Max_Sprintspeed
+#Than we have an else statement (so if the shift key is not being pressed), we change the speed 
+#back to the Max_Walkspeed and if it is larger than the max
 		else:
 			speed = Max_Walkspeed
-			if speed > Max_Walkspeed:
-				speed = Max_Walkspeed
-
-		var motion = move_direction.normalized() * speed
-		move_and_slide(motion)
+#Here we are setting a new variable called motion, motion variable stores the move_direction
+#and then the move_direction is normalized(). normalized() means that the vector is only set
+#to a length of 1, not doing this means that if we wanted to go diagonal, we would go faster
+#we than also times this by the speed in order for us to be able to move
+	var motion = move_direction.normalized() * speed
+#the move_and_slide() function allows kinematic_bodies to stop when a collision occurs, the
+#move and slide function directly influences our motion variable
+	move_and_slide(motion)
 
 func Aim(delta):
+	#this is an inbuilt function within godot, first we get the node that we want to rotate
+	#which in this case is the Head, than we simply state the look_at() function, within 
+	#this function we have another function that records the position of the mouse, with 
+	#these two functions the Head will now look to where to mouse is on the screen
 	$Head.look_at(get_global_mouse_position())
 
+#here we have an if statement that waits for the left mouse button to be pressed
+#however using && we set another paremeter that needs to be met before this if
+#statement can work, our global variable BluePortal has to be equal to true
 	if Input.is_action_just_pressed("PortalBlue") && Global.BluePortal == true:
+#if the if the statement parameters are met than call upon the Shoot() function
 		Shoot()
 
 func Shoot():
+	#We firstly set the global variable BluePortal to false
 	Global.BluePortal = false
+#We create a new variable and call upon the packed scene variable we created earlier,
+#the instance function that we use allows us to instantiate the BlueBullet scene into
+#the current scene, basically copying all of the configuration from the BlueBullet
+#scene  
 	var blue_instance = BlueBullet.instance()
+#We than add the child as a child of our root node for the scene
 	get_parent().add_child(blue_instance)
+#We want the bullet to spawn at the tip of the gun so we get the EndGun variable
+#that is linked to a transform2D and we set it so that the BlueBullet scene is 
+#instantiated at the transfrom2D position
 	blue_instance.global_position = EndGun.global_position
 
+#we than give the bullet velocity towards the 
 	blue_instance.velocity = get_global_mouse_position() - blue_instance.position
 
 ##func Animation(delta):
